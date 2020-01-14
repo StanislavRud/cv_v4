2e4fbf210682b3f1b95675549398def4fcacc0ba
Stanislav <s.rud@factor.corp>
2020-01-14 17:38:20 +0200

    slider

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let nextSlideButton = document.querySelector('.nextSlide');
 let img = document.querySelector('.sliderImg');
 let arrImg = ["./img/elem1.jpg", "./img/elem2.jpg", "./img/elem3.jpg", "./img/elem4.jpg" ];
 let currentImg = 0;
+
 function clickNext () {
     function makeCurrentImgNext() {
         return function () {
@@ -18,9 +19,10 @@ function clickNext () {
     console.log(img.src);
 }
 
+
 function clickPrev (){
     function makeCurrentImgPrev() {
-        if (currentImg <= 0) {
+        if (currentImg === 0) {
             return currentImg = 3;
         } else return function () {
             return currentImg--;
@@ -31,6 +33,8 @@ function clickPrev (){
     img.src = arrImg[curImg()];
     console.log(img.src);
 }
+
+
 
 nextSlideButton.addEventListener('click', clickNext);
 prevSlideButton.addEventListener('click', clickPrev);
